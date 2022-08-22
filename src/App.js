d6215cc64589ae4115a68ff7352ce9a7d545ee72
anandyani <yanianand571@gmail.comm>
2022-08-22 16:09:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useFormik } from 'formik';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  const [bool,setBool] = useState(0);
+  const formik = useFormik({
+    initialValues : {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      confirmpassword: '',
+    },
+
+    validate,
+
+
+    onSubmit : (values , {resetForm}) =>{
+      if(bool){
+        setBool(0);
+        resetForm({values : ''});
+      }
+      else{
+        setBool(1);
+        console.log(values);
+      }
+    }
+  
+});
+
+return(
+<div className="main">
+      <div className="SignUp-form">
+        <h2>Sign Up</h2>
+        <form>
+          <input type="text" placeholder="First Name..." name="firstname" autoComplete="off"/>
+          
+
+
+          <input type="text" placeholder="Last Name..." name="lastname" autoComplete="off"  />
+         
+
+
+
+          <input type="text" placeholder="Email..." name="email" autoComplete="off"  />
+         
+
+
+
+          <input type="password" placeholder="Password..." name="password" autoComplete="off" />
+         
+
+
+          <input type="password" placeholder="Confirm Password..." name="confirmpassword" autoComplete='off'/>
+         
+
+
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    
     </div>
   );
 }
+
+
 
 export default App;
